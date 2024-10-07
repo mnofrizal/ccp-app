9e7ca50bbd38d19742f1e7b3a6cd9c168289f426
mnofrizal <mnofrizal1996@gmail.com>
2024-10-07 10:14:29 +0700

    ok

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  webpack: (config, { isServer }) => {
+    // Tambahkan ini untuk logging
+    console.log("Webpack build:", isServer ? "server" : "client");
+    return config;
+  },
 };
 
 module.exports = nextConfig;
